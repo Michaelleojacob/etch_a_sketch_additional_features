934f59b0006b56ce399f5adb3dde4fbcb6f8e243
Michael <michaelleojacob@gmail.com>
2021-01-16 11:00:40 -0800

    successfully implimented a version where I can use an array to fill the grid.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const gridContainer = document.querySelector('.gridContainer');
 let griditem;
 const resetbtn = document.querySelector('#reset');
+let gridItemArray = [];
 
 for(i=0; i<256; i++){
     const griditem = document.createElement(`div`);
     griditem.classList.add('griditem');
-    gridContainer.appendChild(griditem);
+    gridItemArray.push(griditem);
+    gridItemArray.forEach(item => {
+        gridContainer.appendChild(item)
+    })
     
     
     // !two addEvents. One toggles, the other is a permanent change
