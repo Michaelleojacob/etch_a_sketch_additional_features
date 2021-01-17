82f12a7bf5daa47cabc3cb2f37672bb09575c237
Michael <michaelleojacob@gmail.com>
2021-01-16 16:34:26 -0800

    grid is now being dynamically created using a variable in javascript based on the slider value from user input. currently i'm just hosting the creation of divs statically. only 256 are ever available right now. current goal is to make divs dynamically based on slider value

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 let slider = document.querySelector('#slider');
 let displaySliderValue = document.querySelector('.displaySliderValue');
 let userInputFromSlider;
+let gridContainer = document.querySelector('.gridContainer');
+let gridItemArray = [];
 
 slider.oninput = function(){
     displaySliderValue.textContent = this.value;
     userInputFromSlider = parseInt(this.value);
+    makeGrid();
     return userInputFromSlider;
+};
+
+
+
+
+
+function makeGrid(){
+    document.getElementById('gridContainer').style.setProperty(`grid-template-columns`, `repeat(${userInputFromSlider}, 1fr)`);
+    document.getElementById('gridContainer').style.setProperty(`grid-template-rows`, `repeat(${userInputFromSlider}, 1fr)`);
+}
+
+for(i=0; i<256; i++){
+    const griditem = document.createElement(`div`);
+    griditem.classList.add('griditem');
+    gridItemArray.push(griditem);
+    gridItemArray.forEach(item => {
+        gridContainer.appendChild(item)
+    })
 };
 
 displaySliderValue.textContent = parseInt(slider.value);
